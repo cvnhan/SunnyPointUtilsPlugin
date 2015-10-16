34488c78bea9fbcd75186677cf2067c91689af20
NhanCao <caovannhan2002@gmail.com>
2015-10-16 17:13:34 +0700

    release 1

--- src/GetDb.java
+++ src/GetDb.java
@@ -1,90 +1,65 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by NhanCao on 16-Oct-15.
  */
 public class GetDb extends AnAction {
 
+    Runtime runtime = Runtime.getRuntime();
+    String path_plugins = "";
+
     public GetDb() {
         super();
+//            file:/C:/Users/NhanCao/.IntelliJIdea14/config/plugins/SunnyPoint/lib/SunnyPoint.jar!/config.txt
+//            cmd.exe /c cd C:\Users\NhanCao\.IntelliJIdea14\config\plugins\SunnyPoint\lib && jar xf SunnyPoint.jar sqlitebrowser
+        try {
+            path_plugins = this.getClass().getResource("config.txt").getPath().replace("file:/", "").replace("SunnyPoint.jar!/config.txt", "");
+            //extract sqlitebrowser in jar file
+            File f = new File(path_plugins+"sqlitebrowser");
+            if (!f.exists()) {
+                runtime.exec("cmd.exe /c cd " + path_plugins + " && jar xf SunnyPoint.jar sqlitebrowser");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void actionPerformed(AnActionEvent event) {
         try {
-            Project project = event.getData(PlatformDataKeys.PROJECT);
-
-            String path=project.getBasePath()+"\n";
-            path+=project.getProjectFilePath() + "\n";
-            path+=project.getWorkspaceFile().getPath()+"\n";
-            path+= this.getClass().getClassLoader().getResource("test.txt").getPath()+"\n";
-            path+= this.getClass().getResource("test.txt").getFile()+"\n";
-            path+= this.getClass().getClassLoader().getResource("test.txt").getHost()+"\n";
-            path+= this.getClass().getClassLoader().getResource("test.txt").toURI()+"\n";
-
-
-
-
-            Messages.showMessageDialog(project, path, "Information", Messages.getInformationIcon());
-
-            String s= null;
-            try {
-                InputStream input = this.getClass().getResourceAsStream("test.txt");
-                Scanner scanner = new Scanner(input);
-                s = "";
-                while(scanner.hasNext()){
-                    s+=scanner.nextLine()+"\n";
-                }
-
-                InputStream is = this.getClass().getClassLoader().getResourceAsStream("test.txt");
-                BufferedReader in = new BufferedReader(new InputStreamReader(is));
-                String str;
-                while ((str = in.readLine()) != null) {
-                    s+=str+"\n";
-                }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            Messages.showMessageDialog(project, s, "Information", Messages.getInformationIcon());
-
-            Process process = Runtime.getRuntime().exec("./Sqliteman-1.2.2/sqliteman.exe");
-//            Process process = Runtime.getRuntime().exec(path + "sqliteman.exe " + path + "main.db");
-//            watch(process);
+//            Project project = event.getData(PlatformDataKeys.PROJECT);
+//            String s = null;
+//            try {
+//                InputStream input = this.getClass().getResourceAsStream("config.txt");
+//                Scanner scanner = new Scanner(input);
+//                s = "";
+//                while (scanner.hasNext()) {
+//                    s += scanner.nextLine() + "\n";
+//                }
+//                Messages.showMessageDialog(project, s, "Information", Messages.getInformationIcon());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                Messages.showErrorDialog(e.toString(), "error");
 //
-
-//            String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-//            Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
-
-
-
+//            }
+//            Runtime.getRuntime().exec("adb pull sdcard/main.db " + path_plugins + "main.db");
+            File f = new File(path_plugins+"sqlitebrowser");
+            if (f.exists() && f.isDirectory()) {
+                runtime.exec("TASKKILL /F /IM sqliteman.exe");
+                runtime.exec(path_plugins + "sqlitebrowser/getdb.exe " + path_plugins);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
+            Messages.showErrorDialog(e.toString(), "error");
+
         }
 
-    }
-    private static void watch(final Process process) {
-        new Thread(new Runnable() {
-            public void run() {
-                BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line = null;
-                try {
-                    while ((line = input.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 
 }
